c51d3c35388fb916d517283e7ef672315b5beabb
olgakokh <karalainn@mail.ru>
2022-03-26 13:43:56 +0300

    Add transition from the main page to the login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -10,15 +10,15 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
+        # проверкa на корректный url адрес
         login_url = self.browser.current_url
         assert "login" in login_url, "Error in URL ADRESS"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
+        # проверкa, что есть форма логина
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Error in LOGIN FORM"
       
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
+        # проверкa, что есть форма регистрации на странице
          assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Error in REGISTER FORM"
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,11 +1,15 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
+from .login_page import LoginPage
 
 class MainPage(BasePage): 
     def go_to_login_page(self):
-        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        login_link.click()
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
+        # return LoginPage(browser=self.browser, url=self.browser.current_url) 
+        # login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        # login_link.click()
        
        
     def should_be_login_link(self):
