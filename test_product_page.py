64ee2ea3dafea45a902d00e40c743e01c36854d6
olgakokh <101939104+olgakokh@users.noreply.github.com>
2022-03-31 23:55:46 +0300

    Edit links

--- test_product_page.py
+++ test_product_page.py
@@ -47,7 +47,7 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.should_disappeared_success_message() 
 
 def test_guest_should_see_login_link_on_product_page(browser):
-    link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    link = "https://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
@@ -64,14 +64,14 @@ def test_guest_can_add_product_to_basket(browser):
 
 @pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    link = "https://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
 @pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    link = "https://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_button_basket()
@@ -83,7 +83,7 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
 class TestUserAddToBasketFromProductPage():
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser, email=None, password=None):
-        login_link = "https://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+        login_link = "https://selenium1py.pythonanywhere.com/accounts/login/"
         page = LoginPage(browser, login_link)
         page.open()
         page.register_new_user(email, password)
